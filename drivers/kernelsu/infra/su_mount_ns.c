cda10f0944d671331aa7d22645bf87f4eefaa0e3
hrihq <heriilhamalviansyah28@gmail.com>
2026-09-24 05:39:37 +0800

    fix: do_mount + sys_setns compat for 4.14 link errors

--- drivers/kernelsu/infra/su_mount_ns.c
+++ drivers/kernelsu/infra/su_mount_ns.c
@@ -25,14 +25,21 @@
 #include "infra/su_mount_ns.h"
 #include "util.h"
 
-extern int path_mount(const char *dev_name, struct path *path,
-                      const char *type_page, unsigned long flags,
-                      void *data_page);
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 0, 0))
+// 4.14: do_mount is the exported helper (path_mount is internal in 5.x)
+extern long do_mount(const char *dev_name, const char __user *dir_name,
+                     const char *type_page, unsigned long flags, void *data_page);
+#define path_mount(dev, path, type, flags, data)     do_mount(dev, ((const char __user *)(path)), type, flags, data)
+#endif
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0))
 #if defined(__aarch64__)
 extern long __arm64_sys_setns(const struct pt_regs *regs);
 #elif defined(__x86_64__)
 extern long __x64_sys_setns(const struct pt_regs *regs);
+#endif
+#else
+extern long sys_setns(int fd, int nstype);
 #endif
 
 static long ksu_sys_setns(int fd, int flags)
@@ -43,12 +50,16 @@ static long ksu_sys_setns(int fd, int flags)
     PT_REGS_PARM1(&regs) = fd;
     PT_REGS_PARM2(&regs) = flags;
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0))
 #if defined(__aarch64__)
     return __arm64_sys_setns(&regs);
 #elif defined(__x86_64__)
     return __x64_sys_setns(&regs);
 #else
 #error "Unsupported arch"
+#endif
+#else
+    return sys_setns(fd, flags);
 #endif
 }
 
