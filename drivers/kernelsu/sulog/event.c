623730bfe334d868a3dd0a6dd72f2852f07bea81
hrihq <heriilhamalviansyah28@gmail.com>
2026-09-24 04:05:12 +0800

    fix: include version.h before KERNEL_VERSION use in event.c

--- drivers/kernelsu/sulog/event.c
+++ drivers/kernelsu/sulog/event.c
@@ -1,11 +1,14 @@
 #include <asm/current.h>
+#include <linux/version.h>
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 0, 0))
+#include <linux/kernel.h> // min/max pre-5.0
+#else
+#include <linux/minmax.h>
+#endif
 #include <linux/compat.h>
 #include <linux/cred.h>
 #include <linux/gfp.h>
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 0, 0))
-#include <linux/kernel.h> // min/max in kernel.h pre-5.0
 #else
-#include <linux/minmax.h>
 #endif
 #include <linux/overflow.h>
 #include <linux/sched/signal.h>
