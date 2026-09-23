3b5457773f3bd2da312c31e7b19091f628d553a3
hrihq <heriilhamalviansyah28@gmail.com>
2026-09-24 04:14:30 +0800

    fix: remove leftover guard fragment + dup version.h in event.c

--- drivers/kernelsu/sulog/event.c
+++ drivers/kernelsu/sulog/event.c
@@ -8,15 +8,11 @@
 #include <linux/compat.h>
 #include <linux/cred.h>
 #include <linux/gfp.h>
-#else
-#endif
 #include <linux/overflow.h>
 #include <linux/sched/signal.h>
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/uaccess.h>
-
-#include <linux/version.h>
 #if defined(__x86_64__) && LINUX_VERSION_CODE < KERNEL_VERSION(6, 2, 0)
 #include <linux/mm.h>
 #endif
