aef8d63113f373db0c24865b017cc9e0e0302a16
hrihq <heriilhamalviansyah28@gmail.com>
2026-09-23 16:37:37 +0800

    fix: match ksu_handle_execveat_sucompat symbol name (legacy driver)

--- fs/exec.c
+++ fs/exec.c
@@ -1894,7 +1894,7 @@ int do_execve_file(struct file *file, void *__argv, void *__envp)
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
 #ifdef CONFIG_KSU
-	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+	ksu_handle_execveat_sucompat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
 #endif
 
 
@@ -1904,7 +1904,7 @@ int do_execve_file(struct file *file, void *__argv, void *__envp)
 
 #ifdef CONFIG_KSU
 __attribute__((hot))
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 				void *argv, void *envp, int *flags);
 #endif
 
