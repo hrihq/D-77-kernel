7dd28f2bc6bfb8bc57ab06db141b9ca2e0dc5ab7
hrihq <heriilhamalviansyah28@gmail.com>
2026-09-24 04:47:49 +0800

    fix: define TWA_RESUME as true for 4.14 task_work_add bool arg

--- drivers/kernelsu/supercall/supercall.c
+++ drivers/kernelsu/supercall/supercall.c
@@ -8,6 +8,9 @@
 #include <linux/slab.h>
 #include <linux/syscalls.h>
 #include <linux/task_work.h>
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 0, 0))
+#define TWA_RESUME true // 4.14: task_work_add takes bool
+#endif
 #include <linux/uaccess.h>
 #include <linux/version.h>
 #include <linux/utsname.h> // utsname() and uts_sem
